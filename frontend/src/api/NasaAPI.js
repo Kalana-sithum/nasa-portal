b45d20583a7896e64aba3959e909439561c7a403
Kalana-Sithum <kalanasithum1211@gmail.com>
2024-07-04 12:34:32 +0530

    Update NasaAPI.js

--- frontend/src/api/NasaAPI.js
+++ frontend/src/api/NasaAPI.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 const NasaAPI = {
     getApod: () => {
-        return axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`);
+        return axios.get(`https://api.nasa.gov/planetary/apod?api_key=aQwwEoL9jfY9fmGnIQdzyhpQtHDsitZ58Yd2K3As`);
     },
     getMarsRoverPhotos: () => {
         return axios.get(
-            `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY`
+            `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=aQwwEoL9jfY9fmGnIQdzyhpQtHDsitZ58Yd2K3As`
         );
     },
 };
