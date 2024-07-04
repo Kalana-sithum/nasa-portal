288cb56150ca6f30321f4b45aaf60d213b389ec4
Kalana-Sithum <kalanasithum1211@gmail.com>
2024-07-04 12:30:43 +0530

    update project

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LOGO from '../../asserts/logo.png';
+import LOGO from '../../assets/logo.png';
 
 const Navbar = () => {
     return (
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import COVER from '../../asserts/coverimg.jpg';
+import COVER from '../../assets/coverimg.jpg';
 import Navbar from '../../components/navbar/Navbar';
 
 const Home = () => {
